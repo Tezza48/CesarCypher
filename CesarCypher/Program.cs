cc91f73eaf9a97abec0bff9da4856bcaaf661251
William Terry <Tezza48is@aol.com>
2015-10-16 15:47:44 +0100

    Init Commit
    i made a rhyme

---
+++ CesarCypher/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CesarCypher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int key = 13;
+            char[] input;
+            string output = "";
+            
+
+            //get input string
+            input = Console.ReadLine().ToCharArray();
+            //get bitches
+            //convert to char[]
+            //get bitches
+            //add key to all values
+
+            if (key > 26)
+            {
+                key -= 26;
+            }
+
+            foreach ( char i in input)
+            {
+                //if i > 26 - key
+                if ()
+                {
+                    output += (char)( i + key );
+                }
+            }
+            //get bitches
+            //bring letters that go beyond the alphabet to the start
+            //get bitches
+        }
+    }
+}
